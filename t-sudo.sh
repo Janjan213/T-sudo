ef863bc03731a9b837b0891192aebd307d51a510
John Michael Bicada <46289305+Janjan213@users.noreply.github.com>
2019-02-26 12:55:58 +0800

    Create t-sudo.sh

---
+++ t-sudo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cat>t-sudo.sh
+
+apt update
+apt upgrade
+apt install git
+apt install tsu
+apt install ncurses-utils
+git clone https://gitlab.com/st42/termux-sudo
+cd termux-sudo
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+sudo su
